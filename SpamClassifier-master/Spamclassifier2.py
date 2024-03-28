598be420725815c3f507e8d9fcc6df14749cc6d4
GauravSahu13 <gauravsahu7045@gmail.com>
2024-03-28 13:49:11 +0530

    Spam Mail

---
+++ SpamClassifier-master/Spamclassifier2.py
@@ -0,0 +1,72 @@
+# importing the Dataset
+
+import pandas as pd
+
+messages = pd.read_csv('smsspamcollection/SMSSpamCollection', sep='\t',
+                           names=["label", "message"])
+
+#Data cleaning and preprocessing
+import re
+import nltk
+nltk.download('wordnet')
+
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+lm = WordNetLemmatizer()
+corpus = []
+for i in range(0, len(messages)):
+    review = re.sub('[^a-zA-Z]', ' ', messages['message'][i])       #removing unnecessary character
+    review = review.lower()
+    review = review.split()
+    
+    review = [lm.lemmatize(word) for word in review if not word in stopwords.words('english')]
+    review = ' '.join(review)
+    corpus.append(review)
+    
+    
+# Creating the Bag of Words model
+from sklearn.feature_extraction.text import TfidfVectorizer
+cv = TfidfVectorizer(max_features=5000)
+X = cv.fit_transform(corpus).toarray()
+
+y=pd.get_dummies(messages['label'])         #converting into labels 0 & 1
+y=y.iloc[:,1].values                      #dropping one column
+#dummy variable trap
+
+# Train Test Split
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# Training model using Naive bayes classifier
+
+from sklearn.naive_bayes import MultinomialNB
+spam_detect_model = MultinomialNB().fit(X_train, y_train)
+
+y_pred=spam_detect_model.predict(X_test)
+print("Prediction", y_pred)
+
+
+from sklearn.metrics import confusion_matrix
+conmat = confusion_matrix(y_test,y_pred)
+print("Confusion Metrics\n", conmat)
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(y_test,y_pred)
+print("Accuracy Score",accuracy)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
